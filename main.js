b6c5469cf5e08b9cc9b21f28c07bcebe7e170105
islamnashaat03 <islamnashaat03@gmail.com>
2023-09-01 19:36:17 +0300

    .

--- main.js
+++ main.js
@@ -71,6 +71,30 @@ console.log('================================');
 
 console.log('================================');
 // 6
+switch (new Date().getDay()) {
+  case 0:
+    day = 'Sunday';
+    break;
+  case 1:
+    day = 'Monday';
+    break;
+  case 2:
+    day = 'Tuesday';
+    break;
+  case 3:
+    day = 'Wednesday';
+    break;
+  case 4:
+    day = 'Thursday';
+    break;
+  case 5:
+    day = 'Friday';
+    break;
+  case 6:
+    day = 'Saturday';
+}
+
+console.log(day);
 console.log('================================');
 // 7
 
