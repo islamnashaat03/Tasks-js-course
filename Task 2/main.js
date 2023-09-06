57ae8a27b9e624765c360a8d5fe5f61c670774b4
islamnashaat03 <islamnashaat03@gmail.com>
2023-09-06 08:29:25 +0300

    finish task 2

--- Task 2/main.js
+++ Task 2/main.js
@@ -1,109 +1,99 @@
 // 1
+console.log('===============1=================');
+function getDayType(day) {
+  switch (day.toLowerCase()) {
+    case 'sunday':
+    case 'monday':
+    case 'tuesday':
+    case 'wednesday':
+    case 'thursday':
+      return 'Weekday';
 
-var x = '10';
-console.log(+x);
-console.log('================================');
+    case 'friday':
+    case 'saturday':
+      return 'Weekend';
 
-// 2
-
-var candies = {
-  chocolate: 10,
-  lollipops: null,
-  sourStrips: undefined,
-  jellyBeans: 8,
-  licorice: null,
-};
-var gummyBeras = 1;
-function checkCandies() {
-  console.log(candies.chocolate ?? gummyBeras);
-  console.log(candies.lollipops ?? gummyBeras);
-  console.log(candies.sourStrips ?? gummyBeras);
-  console.log(candies.jellyBeans ?? gummyBeras);
-  console.log(candies.licorice ?? gummyBeras);
-}
-checkCandies();
-
-console.log('================================');
-
-// 3
-var shawarma = {
-  meat: 2,
-  chick: 3,
-};
-
-function getShawarma(name, age) {
-  if (age > 20) {
-    console.log(shawarma.meat);
-  } else {
-    console.log(shawarma.chick);
+    default:
+      return 'Invalid day';
   }
 }
+console.log(getDayType('friday'));
+console.log(getDayType('Saturday'));
+console.log(getDayType('SUNDAY'));
+console.log(getDayType('monday'));
+console.log(getDayType('tuesday'));
+console.log(getDayType('wednesday'));
+console.log(getDayType('thursday'));
+console.log(getDayType('invalid'));
+console.log('================2================');
+// 2
 
-getShawarma('islam', 30);
-getShawarma('ali', 20);
+var developer = {
+  name: 'islam nashaat',
+  age: 30.9,
+  gender: 'male',
+};
 
-console.log('================================');
+var devName = developer.name.toUpperCase();
+var devAge = parseInt(developer.age);
+var devGender = developer.gender;
+console.log(`Developer Name is: ${devName}`);
+console.log(`Developer Age is: ${devAge}`);
+console.log(`Developer Gender is: ${devGender}`);
+console.log('================3================');
+
+// 3
+var shoppingCart = {
+  mobile: 1,
+  items: {
+    apple: 5,
+    banana: 3,
+  },
+  money: {
+    dollars: 900,
+    ryal: 1500,
+    egyptianPound: 1000,
+  },
+};
+
+console.log(shoppingCart.items.apple + ' Apples');
+console.log(shoppingCart.items.banana + ' Bananas');
+console.log(shoppingCart.mobile);
+console.log(`${shoppingCart.money.dollars}$`);
+console.log(`${shoppingCart.money.ryal}Sar`);
+console.log(`${shoppingCart.money.egyptianPound}LE`);
+console.log('================4================');
+
 // 4
-function calculate(num1, num2, operator) {
-  if (operator == '+') {
-    var result = num1 + num2;
-    console.log(result);
-  } else if (operator == '-') {
-    var result = num1 - num2;
-    console.log(result);
-  } else if (operator == '*') {
-    var result = num1 * num2;
-    console.log(result);
-  } else if (operator == '/') {
-    var result = num1 / num2;
-    console.log(result);
-  } else operator ?? console.log('Unknown Method, Try Agin');
-}
-
-calculate(5, 2, '+');
-calculate(5, 2, '-');
-calculate(5, 2, '*');
-calculate(5, 2, '/');
-calculate(5, 2);
-console.log('================================');
-
+console.log('1. Object Literals');
+console.log('var objectx{\nfirstName:"Ali"\nlastName:"Nashaat"\n}');
+console.log(' Ali Nashaat');
+console.log('2. Object.create Method');
+console.log('var newObject = Object.create(objectx)');
+console.log("newObject.firstName = 'Islam'");
+var objectx = {
+  firstName: 'ali',
+  lastName: 'Nashaat',
+};
+var newObject = Object.create(objectx);
+newObject.firstName = 'Islam';
+var fullName = `${newObject.firstName} ${newObject.lastName}`;
+console.log(fullName);
+console.log('================5================');
 // 5
+const sentence = 'The quick brown fox jumps over the lazy dog';
+console.log(sentence.includes('fox'));
 
-console.log('================================');
+console.log('=================6===============');
 // 6
-switch (new Date().getDay()) {
-  case 0:
-    day = 'Sunday';
-    break;
-  case 1:
-    day = 'Monday';
-    break;
-  case 2:
-    day = 'Tuesday';
-    break;
-  case 3:
-    day = 'Wednesday';
-    break;
-  case 4:
-    day = 'Thursday';
-    break;
-  case 5:
-    day = 'Friday';
-    break;
-  case 6:
-    day = 'Saturday';
-}
+const numbers = [1, 2, 3, 4, 5];
+numbers.map((number) => {
+  console.log(number * 10);
+});
+// console.log(numbers.filter((number) => number >= 4));
 
-console.log(day);
-console.log('================================');
-// 7
-
-function getFee(isMember) {
-  return isMember ? '$2' : '$10';
-}
-
-console.log(getFee(true));
-console.log(getFee(false));
-console.log('================================');
-
-object.assign();
+var numberFilter = numbers.filter(function (number) {
+  return number >= 4;
+});
+console.log(numberFilter);
+console.log('=================END===============');
